28d4ae0f62ae66b007b94a18297d62b4b1d52412
Karim Shakirov <kkarimyc779@gmail.com>
2024-09-24 15:16:50 +0300

    Добавьте файлы проекта.

---
+++ CollectionRareCacti/CollectionRareCacti/Windows/ProfileWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using CollectionRareCacti.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CollectionRareCacti.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для ProfileWindow.xaml
+    /// </summary>
+    public partial class ProfileWindow : Window
+    {
+        public ProfileWindow()
+        {
+            InitializeComponent();
+            DataContext = App.contextUser;
+        }
+        private void BEdit_Click(object sender, RoutedEventArgs e)
+        {
+            App.mainFrame.Navigate(new RegistrationPage(App.contextUser));
+            this.Close();
+        }
+        private void BBack_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void BRemove_Click(object sender, RoutedEventArgs e)
+        {
+            App.DB.User.Remove(App.contextUser);
+            App.DB.SaveChanges();
+            App.mainFrame.Navigate(new LoginPage());
+            this.Close();
+
+        }
+    }
+}
